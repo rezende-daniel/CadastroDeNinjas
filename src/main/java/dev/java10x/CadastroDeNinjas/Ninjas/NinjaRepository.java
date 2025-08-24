89407a1e2344d58a6fe7fbfde23fa19c0cd80bd1
Daniel Rezende <rezendedaniel2002@gmail.com>
2025-08-24 19:22:33 -0300

    build:Conecta o banco de dados H2 aminha aplicacao SPRING

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,7 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
+}
