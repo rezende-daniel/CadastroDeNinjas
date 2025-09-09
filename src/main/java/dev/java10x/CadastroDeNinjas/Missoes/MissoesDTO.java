7c8ca7623aa302f414d61bb3ff9076abb4806178
Daniel Rezende <rezendedaniel2002@gmail.com>
2025-09-09 14:27:55 -0300

    feature: Coloca as annotation de contrutor e Get e Set

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesDTO.java
@@ -6,8 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class MissoesDTO {
 
